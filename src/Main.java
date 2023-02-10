120b25481d07691c843f9c485165f61d8590f74e
Vladislav <rk92vlan554@gmail.com>
2023-02-10 20:20:53 +0500

    Для теста второй initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Тренируюсь над тем чтобы создать репозиторий и закинуть его на удаленный репозиторий");
+        System.out.println("Вторая часть теста где я изменяю коммит");
     }
 }
